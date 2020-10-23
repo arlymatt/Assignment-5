19cb8274f336a21fd5cfcb373236a14df2093e3d
Arly Mathew <arlymatt@gmail.com>
2020-10-23 18:14:52 -0400

    Commit Assignment files

---
+++ AssgnmentClass5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace AssgnmentClass5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = null;
+            int value = 0;
+
+            Console.WriteLine("Input Value :");
+            input = Console.ReadLine().ToString();
+            string[] inputString = Regex.Split(input, @"\D+");
+
+            foreach (var count in inputString)
+            {
+                if (count.All(Char.IsDigit) && !string.IsNullOrEmpty(count))
+                    value = value + int.Parse(count);
+                else
+                    Console.WriteLine("Input string is not in correct format.");
+            }
+
+            Console.WriteLine("Output:" + value);
+            Console.ReadLine();
+        }
+    }
+}
